a6693169abe9427b6d84977a555db8e5daefe1d6
Maxime MILOIKOVITCH <maxime.miloikovitch@gmail.com>
2026-06-10 17:01:18 +0200

    ee

--- scripts/start-proxy.sh
+++ scripts/start-proxy.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+set -e
+cd "$(dirname "$0")/.."
 java -cp "build/classes:lib/*" sae.ProxyServer 8000
--- scripts/start-rmi.sh
+++ scripts/start-rmi.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+set -e
+cd "$(dirname "$0")/.."
 java -Djava.rmi.server.hostname=localhost -cp "build/classes:lib/*" sae.ServeurRestaurant 1099 ServiceRestaurant
